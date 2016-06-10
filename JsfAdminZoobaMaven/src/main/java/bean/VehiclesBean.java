dfcc2c12047c3da71f85dc406fed5a37d8d7adcd
Riham <Riham@Riham-PC>
2016-06-10 14:13:28 -0700

    Show Cars Added

--- JsfAdminZoobaMaven/src/main/java/bean/VehiclesBean.java
+++ JsfAdminZoobaMaven/src/main/java/bean/VehiclesBean.java
@@ -5,60 +5,88 @@
  */
 package bean;
 
-
+import reham.DataLayer;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.model.ArrayDataModel;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
-import pojo.Make;
-import webServicesHandlers.Handler;
+import pojo.ModelFeaturesValues;
+import pojo.VehicleModel;
 
 /**
  *
  * @author yoka
  */
-@ManagedBean(name="vehicle")
+@ManagedBean(name="vehicle",eager=true)
 @SessionScoped
-public class VehiclesBean {
- ArrayList<Make> vehicles;
-Handler handler=new Handler();
-    public ListDataModel getVehicles() {
-        return data;
+public class VehiclesBean implements Serializable{ 
+ ArrayList<String> vehicles;
+// Handler handler=new Handler();
+ DataLayer handler=new DataLayer();
+ private String name;
+ private String action;
+ private transient DataModel<Object[]> model=new ListDataModel<>(handler.getVehicles());
+ private transient DataModel<ModelFeaturesValues> list=new ListDataModel<>();
+
+    public DataModel<ModelFeaturesValues> getList() {
+        return list;
     }
-        ListDataModel data ;
-        
-    /**
-     * Creates a new instance of VehiclesBean
-     */
-    public VehiclesBean() {
-//     vehicles=new ArrayList<>();
-//     vehicles.add("e1");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-//     vehicles.add("e2");
-     
-//     List<Make> listmakes=handler.getVehicle();
-    
-//     vehicles=new ArrayList<>(listmakes);
-//        data=new ListDataModel(vehicles) ;
-////      
-//System.out.println("vehicles : "+listmakes.get(0));
-        }
-    
+
+    public void setList(DataModel<ModelFeaturesValues> list) {
+        this.list = list;
     }
     
+    public DataModel<Object[]> getModel() {
+        return model;
+    }
+
+    public void setModel(DataModel<Object[]> model) {
+        this.model = model;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(ArrayList<String> vehicles) {
+        this.vehicles = vehicles;
+    }
+    public void deleteVehicle(String name) {
+      
+        handler.deleteVehicle(name);
+        model=new ListDataModel<>(handler.getVehicles());
+    }
+    
+    public String showPage()
+    {
+        Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        action = params.get("id");
+        System.out.println("Hellooooooo"+action);
+        int n=Integer.parseInt(action);
+        list=new ListDataModel<>(handler.showFeatures(n));
+        return "ShowFeatures";
+    }
+   public void updateFeatures(ModelFeaturesValues m)
+   {
+       handler.update(m);
+       int n=Integer.parseInt(action);
+       list=new ListDataModel<>(handler.showFeatures(n));
+   }
+}
 
