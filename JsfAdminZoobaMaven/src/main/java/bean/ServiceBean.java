12ddfa86b8e2422132b3b23bcc94c6faea76dd97
Ehab-Kamil <Ehabkamil2@gmail.com>
2016-06-23 14:18:08 +0200

    last Commit

--- JsfAdminZoobaMaven/src/main/java/bean/ServiceBean.java
+++ JsfAdminZoobaMaven/src/main/java/bean/ServiceBean.java
@@ -12,6 +12,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import pojo.Service;
 import facadePkg.DataLayer;
+import java.util.HashSet;
+import java.util.Set;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 
@@ -19,29 +21,35 @@ import javax.faces.model.ListDataModel;
  *
  * @author Riham
  */
-@ManagedBean(name="services")
+@ManagedBean(name = "services")
 @SessionScoped
-public class ServiceBean implements Serializable{
-    DataLayer handler=new DataLayer();
+public class ServiceBean implements Serializable {
+
+    DataLayer handler = new DataLayer();
     private List<Service> serviceList;
     private String name;
-    List<String> names=new ArrayList();
- private DataModel<Service> services;
-  
+    List<String> names;
+    private DataModel<Service> services;
+
     public ServiceBean() {
-     serviceList=handler.getServices();
-     
-      services=new ListDataModel<>(serviceList);
+
+        names = new ArrayList();
+        serviceList = handler.getServices();
+        services = new ListDataModel<>(serviceList);
     }
 
-     public List<String> getServiceNames() {
-       serviceList=handler.getServices();
-       for(Service s:serviceList)
-       {
-            name=s.getName();
+    public List<String> getServiceNames() {
+        serviceList = handler.getServices();
+        for (Service s : serviceList) {
+            name = s.getName();
             names.add(name);
-       }     
-       return names;
+        }
+
+        Set<String> hs = new HashSet<>();
+        hs.addAll(names);
+        names.clear();
+        names.addAll(hs);
+        return names;
     }
 
     public DataModel<Service> getServices() {
@@ -50,5 +58,12 @@ public class ServiceBean implements Serializable{
 
     public void setServices(DataModel<Service> services) {
         this.services = services;
+    }
+
+    public void onLoad() {
+        names = new ArrayList();
+        serviceList = handler.getServices();
+        services = new ListDataModel<>(serviceList);
+
     }
 }
